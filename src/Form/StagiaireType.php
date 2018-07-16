b2b1b6480e93c070f0f4426d2602a2907435e9e5
claireb25 <claire.bourgeois.cb@gmail.com>
2018-07-16 14:26:12 +0200

    modification label lieu de naissance du stagiaire

--- src/Form/StagiaireType.php
+++ src/Form/StagiaireType.php
@@ -6,17 +6,23 @@ use App\Entity\Stagiaire;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class StagiaireType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('civilite_id')
             ->add('nom')
             ->add('nom_naissance')
             ->add('prenom')
             ->add('date_naissance')
-            ->add('lieu_naissance')
+            ->add('lieu_naissance', TextType::class, array(
+                'label'=> 'Lieu de naissance',
+            )
+            
+            )
             ->add('adresse')
             ->add('code_postal')
             ->add('commune')
@@ -27,7 +33,6 @@ class StagiaireType extends AbstractType
             ->add('carte_avantages_jeunes')
             ->add('partenaires')
             ->add('adherents')
-            ->add('civilite_id')
             ->add('permis')
             ->add('liaisonStagiaireStageDossierCasBordereau')
         ;
