7a8847b2ad0e67eb72da55ff66a946eec1066ed5
Tom <martint033@gmail.com>
2018-07-17 09:31:28 +0200

    reparation pour de vrai de l'erreur

--- src/Form/StagiaireType.php
+++ src/Form/StagiaireType.php
@@ -11,7 +11,6 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\AbstractType;
 
 class StagiaireType extends AbstractType
 {
