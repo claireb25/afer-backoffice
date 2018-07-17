345c753a33760535aead10e1cc96833e41131048
sirine <sirine.b@codeur.online>
2018-07-17 09:23:30 +0200

    regler bug tom

--- src/Form/StagiaireType.php
+++ src/Form/StagiaireType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Stagiaire;
 use App\Entity\Civilite;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
