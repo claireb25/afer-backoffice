7af6720dabdf7f72e2d07caeadbfb51d49086822
sirine <sirine.b@codeur.online>
2018-07-17 08:50:43 +0200

    modif 1/2 page stagiaire

--- src/Form/StagiaireType.php
+++ src/Form/StagiaireType.php
@@ -3,7 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Stagiaire;
+use App\Entity\Civilite;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +17,25 @@ class StagiaireType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('civilite_id', EntityType::class,
+            array(  'class' => Civilite::class,
+                    'label' => 'Civilité'
+            ,))
             ->add('nom')
-            ->add('nom_naissance')
-            ->add('prenom')
-            ->add('date_naissance')
-            ->add('lieu_naissance')
+            ->add('nom_naissance', TextType::class,
+            array ( 'label' => 'Nom de naissance'
+            ,))
+            ->add('prenom', TextType::class,
+            array ( 'label' => 'Prénom'
+            ,))
+            ->add('date_naissance', DateType::class,
+            array(  'label' => 'Date de naissance',
+                    'widget' => 'choice',
+                    'years' => range(date('Y'), date('Y')-100)
+            ,))
+            ->add('lieu_naissance', TextType::class,
+            array ( 'label' => 'Ville de naissance'
+            ,))
             ->add('adresse')
             ->add('code_postal')
             ->add('commune')
@@ -27,7 +46,6 @@ class StagiaireType extends AbstractType
             ->add('carte_avantages_jeunes')
             ->add('partenaires')
             ->add('adherents')
-            ->add('civilite_id')
             ->add('permis')
             ->add('liaisonStagiaireStageDossierCasBordereau')
         ;
